2dd6e0e177929b0cd74db268a2e1759c0a9b53ba
Beslan <beslandudaev8@gmail.com>
2025-02-18 23:24:05 +0300

    настроил основную страницу

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-import './style.css'
+import { createHeader } from './header.js'
+import { createHomePage } from './home.js'
+
+
+const root = document.querySelector('.root')
+
+root.append(createHeader(), createHomePage())
