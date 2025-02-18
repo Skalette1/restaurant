62595f7180dff55096f3f65d7ebf36a6170bb43c
Beslan <beslandudaev8@gmail.com>
2025-02-18 14:04:38 +0300

    настроил среду

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path')
+
+module.exports = {
+    entry: './main.js',
+    output: {
+        filemane: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
+    },
+    plugins: [new HtmlWebpackPlugin()],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            }
+        ]
+    }
+}
