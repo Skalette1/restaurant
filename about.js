00717ee7e40c5eb1480731ed0e982ebb87e25abb
Jamal <dsardalov1@gmail.com>
2025-02-23 22:41:55 +0300

    завершил основные страницы

---
+++ about.js
@@ -0,0 +1,30 @@
+export const createAbout = () => {
+
+const about = document.querySelector('.about')
+
+const aboutContainer = document.createElement('div')
+aboutContainer.className = 'aboutContainer'
+
+const aboutMenu = document.createElement('div')
+aboutMenu.className = 'aboutMenu'
+
+const aboutTitle = document.createElement('h1')
+aboutTitle.textContent = 'Hamburger story'
+
+const aboutText = document.createElement('p')
+aboutText.textContent = 'A hamburger, or simply a burger, is a dish consisting of fillings—usually a patty of ground meat, typically beef—placed inside a sliced bun or bread roll. The patties are often served with cheese, lettuce, tomato, onion, pickles, bacon, or chilis with condiments such as ketchup, mustard, mayonnaise, relish or a "special sauce", often a variation of Thousand Island dressing, and are frequently placed on sesame seed buns. A hamburger patty topped with cheese is called a cheeseburger. Under some definitions, and in some cultures, a burger is considered a sandwich.'
+aboutText.className = 'aboutText'
+
+const aboutImage = document.createElement('img')
+aboutImage.src = './images/hamburger2.png'
+aboutImage.className = 'aboutImage'
+
+aboutMenu.append(aboutTitle, aboutText, aboutImage)
+
+aboutContainer.append(aboutMenu)
+
+about.append(aboutContainer)
+
+return about
+
+}
